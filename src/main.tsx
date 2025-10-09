1e99c38def3eac02fda5d3c3cc1649d052c8b965
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-09 15:06:48 +0700

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from '@/components/theme-provider';
 import { App } from '@/app';
-import { ModeToggle } from "./components/mode-toggle";
+import { BrowserRouter } from 'react-router';
+import { Navbar } from './components/navbar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-<ModeToggle />
-    <App />
-
-    </ThemeProvider>
-  </StrictMode>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Navbar />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
