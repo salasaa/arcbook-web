9f97c0b92c2855756109d5d7cb9a0ece9d9596ab
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-09 12:00:16 +0700

    feat: setup dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from "@/components/theme-provider";
 import { App } from '@/app';
+import { ModeToggle } from "./components/mode-toggle";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+<ModeToggle />
     <App />
+
+    </ThemeProvider>
   </StrictMode>
 );
