e5d9ddcc46667bf2439a30e3113b958a08bb0e84
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-18 02:14:17 +0700

    feat: add loading spinner

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import { Loader2 } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,10 +11,52 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+type Product = {
+  id: string;
+  slug: string;
+  title: string;
+  author: string;
+  price: number;
+  priginalPrice: number;
+  description?: string | null;
+  imageUrl: string;
+  inStock: boolean;
+  publishYear: number;
+
+  categoryId: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type Products = Product[];
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const response = await fetch(`http://localhost:3000/products`);
+  const products: Products = await response.json();
+  return products;
+}
+
+export function HydrateFallback() {
+  return (
+    <div className="flex flex-col items-center justify-center p-8 space-y-3">
+      <Loader2 className="h-8 w-8 text-primary animate-spin" />
+      <div className="text-lg font-medium text-gray-600 dark:text-gray-400">
+        Loading products...
+      </div>
+    </div>
+  );
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const products = loaderData;
+
+  console.log({ products });
+
   return (
     <div>
-      <h1>Arcbooks</h1>
+      <h1>Disini halaman Arcbooks</h1>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </div>
   );
 }
